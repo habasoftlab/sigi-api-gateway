3a4d4142c0909643380aef90c84556d4e5a9aec6
habacucsoto <josehabacuc182020@gmail.com>
2026-06-11 10:12:36 -0600

    add configuration paths

--- src/test/java/com/servispeed/gateway/ApiGatewayApplication.java
+++ src/test/java/com/servispeed/gateway/ApiGatewayApplication.java
@@ -1,29 +1,14 @@
+
 package com.servispeed.gateway;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-import com.servispeed.gateway.filter.SecurityFilter;
-
-@EnableZuulProxy
-@SpringBootApplication
-public class ApiGatewayApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
-	
-	@Bean
-	public SecurityFilter preFilter() {
-		return new SecurityFilter();
-	}
-
-	@Bean
-	public RestTemplate restTemplate() {
-	    return new RestTemplate();
-	}
-
+/**
+ * Hello world!
+ *
+ */
+public class ApiGatewayApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
 }
